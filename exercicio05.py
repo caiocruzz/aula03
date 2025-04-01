04b2c9d282b9e401694f10aabfeb0db221f0a62f
Caio Cruz <chvcruz@gmail.com>
2025-04-01 10:52:06 -0300

    exercicio05 caclculando a media

---
+++ exercicio05.py
@@ -0,0 +1,9 @@
+nota1= float(input("Digite a nota do aluno: "))
+nota2= float(input("Digite a nota do aluno: "))
+nota3= float(input("Digite a nota do aluno: "))
+calculoMedia= (nota1+nota2+nota3) / 3
+media= 7.0
+if calculoMedia >= media :
+    print("aprovado")
+else :
+    print("reprovado")
