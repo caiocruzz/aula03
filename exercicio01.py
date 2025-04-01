4ff5a921e9f69d700c09c2c5757b14e73a97b2bf
Caio Cruz <chvcruz@gmail.com>
2025-04-01 08:55:23 -0300

    Primeira task do dia

---
+++ exercicio01.py
@@ -0,0 +1,5 @@
+nome= input("Insira seu nome: ")
+idade= int(input("Qual sua idade ? "))
+salario= float(input("Quanto voce recebe?"))
+
+print(f"Meu nome é: {nome}  Minha idade é: {idade} Meu salário é {salario}")
