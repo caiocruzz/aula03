23da62cf6bc2aa7203ffe776496eeb85019caa5b
Caio Cruz <chvcruz@gmail.com>
2025-04-10 08:22:47 -0300

    correção do exercicio na aula

--- exercicio12.py
+++ exercicio12.py
@@ -4,3 +4,9 @@ for i in range (1,11,1):
     var = var + 1
     calculoTabuada = numero * var
     print(f"{numero} x {i} = {calculoTabuada}")
+
+  # resposta do professor
+num = int(input("Digite um número: "))
+for x in range  (1,11):
+    multi = x * num
+    print(f"{x} * {num} = {multi}")
